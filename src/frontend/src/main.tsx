57ce4f1e54fb9253c4a667acaed4f7267c108afa
lukqw <lukqw@users.noreply.github.com>
2025-11-24 17:41:39 +0100

    chore(path-based-access): switch to HashRouter

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
       <Toaster position="top-center" richColors closeButton />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
